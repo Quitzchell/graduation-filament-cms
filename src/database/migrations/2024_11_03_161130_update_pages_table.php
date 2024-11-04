0a88678381b28f22ac69d8fe88840d2855f6b699
mitchellquitz <mitchell@allesonline.nl>
2024-11-04 13:15:10 +0100

    remove template model

--- src/database/migrations/2024_11_03_161130_update_pages_table.php
+++ src/database/migrations/2024_11_03_161130_update_pages_table.php
@@ -4,21 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('templates', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
-
         Schema::table('pages', function (Blueprint $table) {
-           $table->foreign('template_id')->on('templates')->references('id')->nullOnDelete();
+            $table->json('data')->nullable()->after('template');
         });
     }
 
@@ -27,6 +20,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('templates');
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropColumn('data');
+        });
     }
 };
