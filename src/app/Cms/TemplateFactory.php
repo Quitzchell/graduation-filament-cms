1cf5851a45b9e91c7e2d902c45ea799740cd6937
mitchellquitz <mitchell@allesonline.nl>
2024-11-04 13:14:06 +0100

    update TemplateFactory

--- src/app/Cms/TemplateFactory.php
+++ src/app/Cms/TemplateFactory.php
@@ -13,13 +13,35 @@ class TemplateFactory
         ];
     }
 
-    public static function loadTemplateSchema(string $templateClass): array
+    public static function getTemplateFields(string $template): array
     {
-        if (class_exists($templateClass)) {
-            return (new $templateClass())->getForm();
+        $formSchema = self::loadTemplateSchema($template);
+        return self::extractFieldNames($formSchema);
+    }
+
+    public static function loadTemplateSchema(string $template): array
+    {
+        if (class_exists($template)) {
+            return (new $template())->getForm();
         }
 
         return [];
     }
 
+    protected static function extractFieldNames(array $components): array
+    {
+        $fields = [];
+
+        foreach ($components as $component) {
+            if (method_exists($component, 'getName')) {
+                $fields[] = $component->getName();
+            }
+
+            if (method_exists($component, 'getSchema')) {
+                $fields = array_merge($fields, self::extractFieldNames($component->getSchema()));
+            }
+        }
+
+        return $fields;
+    }
 }
