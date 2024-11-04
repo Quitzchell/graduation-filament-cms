b3d79c0b6a12e1107fd961aaec065c851d742c1b
mitchellquitz <mitchell@allesonline.nl>
2024-11-04 13:17:38 +0100

    allow saving of 'cms-content' as JSON

--- src/app/Filament/Resources/PageResource/Pages/EditPage.php
+++ src/app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Resources\PageResource\Pages;
 
 use App\Filament\Resources\PageResource;
+use App\Filament\Resources\Traits\MutateTemplateData;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPage extends EditRecord
 {
+    use MutateTemplateData;
+
     protected static string $resource = PageResource::class;
 
     protected function getHeaderActions(): array
@@ -15,5 +18,21 @@ class EditPage extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        foreach ($data['data'] as $key => $templateData) {
+            $data[$key] = $templateData;
+        }
+
+        unset($data['data']);
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        return $this->mutateTemplateData($data);
     }
 }
