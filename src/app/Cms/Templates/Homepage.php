55b9a0e3f6a602d00415ca0b4121db950ec3b7dc
mitchellquitz <mitchell@allesonline.nl>
2024-11-04 08:37:14 +0100

    add and allow FileUpload to work

--- src/app/Cms/Templates/Homepage.php
+++ src/app/Cms/Templates/Homepage.php
@@ -5,6 +5,7 @@ namespace App\Cms\Templates;
 use App\Cms\Blocks\Paragraph;
 use App\Cms\Templates\Interfaces\TemplateContract;
 use Filament\Forms\Components\Builder;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 
 class Homepage implements TemplateContract
@@ -13,8 +14,13 @@ class Homepage implements TemplateContract
     public static function getForm(): array
     {
         return [
-            TextInput::make('title')
-                ->label('Title'),
+            FileUpload::make('header_image')
+                ->label('Header image')
+                ->live()
+                ->required(),
+            TextInput::make('header_title')
+                ->label('Header title'),
+
             Builder::make('content')->schema([
                 Paragraph::getBlock()
             ])
--- src/app/Http/Middleware/TrustProxies.php
+++ src/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // todo: inject correct proxies
 
     /**
      * The headers that should be used to detect proxies.
