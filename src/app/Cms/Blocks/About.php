c0911d274b5844c7f0c08be0cd1ff90d2099110d
mitchellquitz <mitchell@allesonline.nl>
2024-11-05 22:12:09 +0100

    add About and CallToAction blocks

---
+++ src/app/Cms/Blocks/About.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Cms\Blocks;
+
+use App\Cms\Blocks\Interfaces\BlockContract;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+
+class About implements BlockContract
+{
+    public static function getBlock(): Block
+    {
+        return Block::make('about')
+            ->schema([
+                TextInput::make('title')
+                    ->label('Title'),
+                RichEditor::make('text')
+                    ->label('Text')
+                    ->toolbarButtons([
+                        'h1',
+                        'h2',
+                        'bold',
+                        'italic',
+                        'underline',
+                        'orderedList',
+                        'bulletList',
+                    ]),
+            ]);
+    }
+}
