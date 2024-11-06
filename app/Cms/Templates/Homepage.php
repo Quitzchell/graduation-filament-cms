f441341edbfe26a907401c6e98bca3e811154ac2
mitchellquitz <mitchell@allesonline.nl>
2024-11-06 08:19:51 +0100

    move project to root and add .dockerignore

---
+++ app/Cms/Templates/Homepage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Cms\Templates;
+
+use App\Cms\Blocks\About;
+use App\Cms\Blocks\CallToAction;
+use App\Cms\Blocks\Image;
+use App\Cms\Blocks\Map;
+use App\Cms\Blocks\Paragraph;
+use App\Cms\Templates\Interfaces\TemplateContract;
+use Filament\Forms\Components\Builder;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+
+class Homepage implements TemplateContract
+{
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('header_title')
+                ->label('Header title'),
+
+            FileUpload::make('header_image')
+                ->label('Header Image')
+                ->image()
+                ->required(),
+
+            Builder::make('content')->schema([
+                About::getBlock(),
+                CallToAction::getBlock(),
+                Image::getBlock(),
+                Map::getBlock(),
+                Paragraph::getBlock(),
+            ]),
+        ];
+    }
+}
---
+++ app/Models/Page.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Interface\UrlableContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+class Page extends Model implements UrlableContract
+{
+    protected $table = 'pages';
+
+    protected $fillable = [
+        'name',
+        'template',
+        'content',
+    ];
+
+    protected $casts = [
+        'content' => 'array',
+    ];
+
+    /* Relations */
+
+    public function urlable(): MorphMany
+    {
+        return $this->morphMany(Urlable::class, 'linkable');
+    }
+
+    /* Urlable */
+    public function uri($lang = null)
+    {
+        return strtolower($this->name);
+    }
+
+    public function url($lang = null)
+    {
+        return config('app.url').'Page.php/'.$this->uri($lang);
+    }
+}
