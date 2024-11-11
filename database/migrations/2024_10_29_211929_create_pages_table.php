2baad9a3ead20cb7533f260f6a02bfe1d523ed50
mitchellquitz <mitchell@allesonline.nl>
2024-11-11 08:34:23 +0100

    update page models

--- database/migrations/2024_10_29_211929_create_pages_table.php
+++ database/migrations/2024_10_29_211929_create_pages_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Page;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,11 +14,16 @@ return new class extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable()->index();
             $table->string('name');
             $table->string('template')->nullable();
             $table->boolean('locked')->default(false);
             $table->timestamps();
             $table->softDeletes();
+        });
+
+        Schema::table('pages', function (Blueprint $table) {
+            $table->foreign('parent_id')->references('id')->on('pages')->cascadeOnDelete();
         });
     }
 
