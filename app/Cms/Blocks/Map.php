6ab9caea919fe2638f34b807276fcca9decdfa84
mitchellquitz <mitchell@allesonline.nl>
2024-11-14 08:19:17 +0100

    configure and implement FilamentGoogleMaps plugin and create Map Block

--- app/Cms/Blocks/Map.php
+++ app/Cms/Blocks/Map.php
@@ -3,7 +3,8 @@
 namespace App\Cms\Blocks;
 
 use App\Cms\Blocks\Interfaces\BlockContract;
-use App\Forms\Components\GoogleMapEmbedField;
+use Cheesegrits\FilamentGoogleMaps\Fields\Map as GoogleMapPicker;
+use Cheesegrits\FilamentGoogleMaps\Fields\Geocomplete;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\TextInput;
 
@@ -18,13 +19,46 @@ class Map implements BlockContract
                     ->label('Title'),
                 TextInput::make('text')
                     ->label('Text'),
-                GoogleMapEmbedField::make('location')
-                    ->label('Location'),
+
+                GoogleMapPicker::make('location')
+                    ->label('Location')
+                    ->geolocateOnLoad()
+                    ->reactive()
+                    ->afterStateHydrated(function ($state, $record, callable $set) {
+                        if ($record) {
+                            $set('location', [
+                                'lat' => $record->location['lat'] ?? null,
+                                'lng' => $record->location['lng'] ?? null,
+                            ]);
+                        }
+                    })
+                    ->mapControls([
+                        'mapTypeControl' => false,
+                        'scaleControl' => false,
+                        'streetViewControl' => false,
+                        'rotateControl' => false,
+                        'fullscreenControl' => false,
+                        'searchBoxControl' => false,
+                        'zoomControl' => false,
+                    ]),
+                Geocomplete::make('address')
+                    ->label('Address')
+                    ->placeholder('Start typing an address...')
+                    ->geocodeOnLoad()
+                    ->isLocation()
+                    ->reactive()
+                    ->afterStateHydrated(function ($state, $record, callable $set) {
+                        if ($record) {
+                            $set('address', $record->address ?? '');
+                        }
+                    })
+                    ->live()
+                    ->afterStateUpdated(function ($state, callable $set) {
+                        if (!empty($state['lat']) && !empty($state['lng'])) {
+                            $set('location.lat', $state['lat']);
+                            $set('location.lng', $state['lng']);
+                        }
+                    }),
             ]);
     }
 }
-
-//         $this->title = $block->content('title');
-//        $this->text = $block->content('text');
-//        $this->location = $block->content('location');
-//        $this->mapKey = config('services.google_maps_key');
