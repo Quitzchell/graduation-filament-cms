2c72bf78dab486786582475c72185923197c8a9b
mitchellquitz <mitchell@allesonline.nl>
2024-11-19 21:13:58 +0100

    create Content model to handle all content

---
+++ app/Models/Content.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Content extends Model
+{
+    protected $table = 'content';
+
+    protected $fillable = [
+        'contentable_type',
+        'contentable_id',
+        'name',
+        'value'
+    ];
+
+    protected $casts = [
+        'value' => 'array'
+    ];
+
+    public function contentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
