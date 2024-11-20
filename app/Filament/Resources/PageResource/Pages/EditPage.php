0e06a8cc9f08b338166cc5659b2fa87bf61ed54e
mitchellquitz <mitchell@allesonline.nl>
2024-11-20 08:52:44 +0100

    update logic for saving CMS Content

--- app/Filament/Resources/PageResource/Pages/EditPage.php
+++ app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -3,14 +3,14 @@
 namespace App\Filament\Resources\PageResource\Pages;
 
 use App\Filament\Resources\PageResource;
-use App\Filament\Resources\Traits\MutateDataBeforeCreateOrUpdateTrait;
 use App\Filament\Resources\Traits\MutateDataBeforeFillTrait;
+use App\Filament\Resources\Traits\MutateDateBeforeSaveTrait;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPage extends EditRecord
 {
-    use MutateDataBeforeCreateOrUpdateTrait;
+    use MutateDateBeforeSaveTrait;
     use mutateDataBeforeFillTrait;
 
     protected static string $resource = PageResource::class;
@@ -20,15 +20,5 @@ class EditPage extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
-    }
-
-    protected function mutateFormDataBeforeFill(array $data): array
-    {
-        return $this->beforeFillMutation($data);
-    }
-
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        return $this->beforeCreateOrUpdateMutation($data);
     }
 }
---
+++ app/Filament/Resources/Traits/MutateDataBeforeCreateTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\Traits;
+
+use App\Cms\TemplateFactory;
+use Illuminate\Support\Arr;
+
+trait MutateDataBeforeCreateTrait
+{
+    private array $cmsContent;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['template'])) {
+            $templateFields = TemplateFactory::getTemplateFields($data['template']);
+            $this->cmsContent = Arr::only($data, $templateFields);
+        }
+        return Arr::except($data, $templateFields ?? []);
+    }
+
+    protected function afterCreate(): void
+    {
+        if ($this->cmsContent) {
+            foreach ($this->cmsContent as $name => $value) {
+                $this->record->contents()->create(['name' => $name, 'value' => $value]);
+            }
+        }
+    }
+}
