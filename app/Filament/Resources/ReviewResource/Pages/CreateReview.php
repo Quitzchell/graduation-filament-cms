9f1a35f55af8c73f62bb58430dbea4cde718672d
mitchellquitz <mitchell@allesonline.nl>
2024-11-25 00:44:53 +0100

    add traits for mutating data for content

--- app/Filament/Resources/ReviewResource/Pages/CreateReview.php
+++ app/Filament/Resources/ReviewResource/Pages/CreateReview.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\ReviewResource\Pages;
 
 use App\Filament\Resources\ReviewResource;
+use App\Filament\Resources\Traits\MutateDataBeforeCreateTrait;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateReview extends CreateRecord
 {
+    use MutateDataBeforeCreateTrait;
+
     protected static string $resource = ReviewResource::class;
 }
--- app/Filament/Resources/ReviewResource/Pages/EditReview.php
+++ app/Filament/Resources/ReviewResource/Pages/EditReview.php
@@ -3,11 +3,16 @@
 namespace App\Filament\Resources\ReviewResource\Pages;
 
 use App\Filament\Resources\ReviewResource;
+use App\Filament\Resources\Traits\MutateDataBeforeFillTrait;
+use App\Filament\Resources\Traits\MutateDateBeforeSaveTrait;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditReview extends EditRecord
 {
+    use mutateDataBeforeFillTrait;
+    use MutateDateBeforeSaveTrait;
+
     protected static string $resource = ReviewResource::class;
 
     protected function getHeaderActions(): array
