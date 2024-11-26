7656aa33b1e00088a9a0ae79a58ab4a408a6eaf4
mitchellquitz <mitchell@allesonline.nl>
2024-11-26 15:03:53 +0100

    clean ProvidesContentTrait

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Models\Interfaces\HasContent;
-use App\Models\Traits\ProvideContent;
+use App\Models\Traits\ProvidesContentTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BlogPost extends Model implements HasContent
 {
-    use ProvideContent;
+    use ProvidesContentTrait;
 
     protected $table = 'blog_posts';
 
