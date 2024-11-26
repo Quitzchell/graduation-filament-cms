880bf1cb98762aa40e03bbcb3847294963023092
Quitzchell <Quitzchell@users.noreply.github.com>
2024-11-26 21:48:29 +0000

    Fixes coding style

--- app/Cms/Blocks/DTO/BlockData.php
+++ app/Cms/Blocks/DTO/BlockData.php
@@ -10,10 +10,8 @@ final readonly class BlockData implements Arrayable, JsonSerializable
     public function __construct(
         private string $template,
         private string $identifier,
-        private array  $data,
-    )
-    {
-    }
+        private array $data,
+    ) {}
 
     public function toArray(): array
     {
