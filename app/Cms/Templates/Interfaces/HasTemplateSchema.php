6f7f5997c14e03e8554acd9980ccafe2022fca3a
mitchellquitz <mitchell@allesonline.nl>
2024-12-06 22:18:18 +0100

    remove Templates enum, add templates through Laravel Service container en create instances on places the Templates are used

--- app/Cms/Templates/Interfaces/HasTemplateSchema.php
+++ app/Cms/Templates/Interfaces/HasTemplateSchema.php
@@ -4,7 +4,7 @@ namespace App\Cms\Templates\Interfaces;
 
 interface HasTemplateSchema
 {
-    public static function getName(): string;
+    public function getName(): string;
 
-    public static function getForm(): array;
+    public function getForm(): array;
 }
