ac59830901aa10f98292bface297c27be03605ef
mitchellquitz <mitchell@allesonline.nl>
2024-12-06 20:22:57 +0100

    update logic to create and manage menu structure for the website

--- app/Filament/Resources/MenuResource/Pages/EditMenu.php
+++ app/Filament/Resources/MenuResource/Pages/EditMenu.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\MenuResource\Pages;
 
 use App\Filament\Resources\MenuResource;
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Support\Str;
@@ -15,12 +14,5 @@ class EditMenu extends EditRecord
     public function getTitle(): string|Htmlable
     {
         return __(ucwords(Str::replaceFirst('Menu', $this->record->name, parent::getTitle())));
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
     }
 }
